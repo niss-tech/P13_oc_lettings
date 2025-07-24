22963a52c372d37bf00da64c2ea41b7587765081
nisrine adamo <nisrineadamo1@gmail.com>
2025-07-24 16:11:05 +0200

    Ajout des pages d'erreur 404 et 500 personnalisées avec configuration ALLOWED_HOSTS

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('lettings/', include('lettings.urls')),
     path('profiles/', include('profiles.urls')),
     path('admin/', admin.site.urls),
+    path('test-404/', views.test_404),
+    path('test-500/', views.test_500),
+
+
 ]
--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponseNotFound
 
 
 def index(request):
     # Affiche la page d’accueil principale du site
     return render(request, 'index.html')
+
+# Vue de test temporaire pour afficher la page d'erreur 404 personnalisée
+def test_404(request):
+    return HttpResponseNotFound(render(request, '404.html'))
+
+# Vue de test temporaire pour déclencher une erreur 500
+def test_500(request):
+    1 / 0  # crash volontaire
+
